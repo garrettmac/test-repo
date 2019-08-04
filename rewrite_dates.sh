86db76f1516fba043b830c6d5685f9b65ea8ffcf
garrettmac <garrettmacmac@gmail.com>
2019-08-03 18:56:39 -0700

    pip

--- rewrite_dates.sh
+++ rewrite_dates.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# Ensure the repository is clean
-if ! git diff-index --quiet HEAD --; then
-  echo "Your repository is not clean. Commit or stash your changes before proceeding."
-  exit 1
+# Ensure git-filter-repo is installed
+if ! command -v git-filter-repo &> /dev/null
+then
+    echo "git-filter-repo could not be found. Please install it using 'pip install git-filter-repo'."
+    exit 1
 fi
 
 # Rewriting commit history
-git filter-branch --env-filter '
-OLD_DATE=$(date -d "$GIT_COMMITTER_DATE - 5 years" +%Y-%m-%dT%H:%M:%S)
-export GIT_COMMITTER_DATE=$OLD_DATE
-export GIT_AUTHOR_DATE=$OLD_DATE
-' -- --all
+git filter-repo --commit-callback '
+import datetime
+
+commit.author_date = str(
+    datetime.datetime.fromtimestamp(commit.author_date) - datetime.timedelta(days=5*365)
+)
+commit.committer_date = str(
+    datetime.datetime.fromtimestamp(commit.committer_date) - datetime.timedelta(days=5*365)
+)
+'
 
 echo "Rewriting commit history completed. Don't forget to force push to your remote repository."
