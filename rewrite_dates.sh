d19347e6a9014934df090ab250b2659f96332e0e
garrettmac <garrettmacmac@gmail.com>
2019-08-03 18:56:39 -0700

    update script

--- rewrite_dates.sh
+++ rewrite_dates.sh
@@ -6,16 +6,17 @@ if ! git diff-index --quiet HEAD --; then
   exit 1
 fi
 
-# Rewriting commit history to set dates to exactly 5 years earlier
+# Logging commit history dates without making any changes
 git filter-branch --env-filter '
 OLD_AUTHOR_DATE=$(git log -1 --pretty=format:%ai)
 OLD_COMMITTER_DATE=$(git log -1 --pretty=format:%ci)
-
 NEW_AUTHOR_DATE=$(date -d "$OLD_AUTHOR_DATE - 5 years" +%Y-%m-%dT%H:%M:%S)
 NEW_COMMITTER_DATE=$(date -d "$OLD_COMMITTER_DATE - 5 years" +%Y-%m-%dT%H:%M:%S)
 
-export GIT_AUTHOR_DATE="$NEW_AUTHOR_DATE"
-export GIT_COMMITTER_DATE="$NEW_COMMITTER_DATE"
+echo "OLD_AUTHOR_DATE: $OLD_AUTHOR_DATE"
+echo "OLD_COMMITTER_DATE: $OLD_COMMITTER_DATE"
+echo "NEW_AUTHOR_DATE: $NEW_AUTHOR_DATE"
+echo "NEW_COMMITTER_DATE: $NEW_COMMITTER_DATE"
 ' -- --all
 
-echo "Rewriting commit history completed. Don't forget to force push to your remote repository."
+echo "Logging commit history dates completed. No changes were made."
